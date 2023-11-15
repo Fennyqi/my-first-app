8fdfaec00363bcc4fccdbb17414ca77f3d81a45c
Fennyqi <yq98@georgetown.edu>
2023-11-15 18:46:11 -0500

    fix test

--- test/weatherapp_test.py
+++ test/weatherapp_test.py
@@ -3,11 +3,10 @@ from app.weatherapp import get_weather_info, display_forecast, main
 
 
 def test_get_weather_info():
-  
-        assert isinstance(result, list)
-        assert len(result) > 0
-        assert isinstance(result[0], dict)
-        assert set(result[0].keys()) == {"name", "startTime", "shortForecast", "temperature", "temperatureUnit", "icon"}
+    result = get_weather_info("20057")
+    assert isinstance(result, list)
+    assert len(result) > 0
+    assert isinstance(result[0], dict)
+    assert set(result[0].keys()) == {"name", "startTime", "shortForecast", "temperature", "temperatureUnit", "icon"}
 
-if __name__ == "__main__":
-    test_get_weather_info()
+
